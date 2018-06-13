e82cfe71f7910177e801457816ebc7db0cc836fd
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2018-06-13 14:48:17 -0400

    test: add incremental utimes testcase

--- test/test-fs.c
+++ test/test-fs.c
@@ -2232,6 +2232,27 @@ TEST_IMPL(fs_utime) {
   uv_run(loop, UV_RUN_DEFAULT);
   ASSERT(utime_cb_count == 1);
 
+  for (int i = 1; i <= 5; i++) {
+    uv_stat_t *s;
+    r = uv_fs_stat(NULL, &req, path, NULL);
+    ASSERT(r == 0);
+    ASSERT(req.result == 0);
+    s = &req.statbuf;
+    double real_atime = s->st_atim.tv_sec + (s->st_atim.tv_nsec / 1000000000.0);
+    double real_mtime = s->st_mtim.tv_sec + (s->st_mtim.tv_nsec / 1000000000.0);
+
+    r = uv_fs_utime(NULL, &req, path, real_atime + i, real_mtime + i, NULL);
+    ASSERT(r == 0);
+    ASSERT(req.result == 0);
+    uv_fs_req_cleanup(&req);
+
+    r = uv_fs_stat(NULL, &req, path, NULL);
+    ASSERT(r == 0);
+    ASSERT(req.result == 0);
+    check_utime(path, real_atime + i, real_mtime + i);
+    uv_fs_req_cleanup(&req);
+  }
+
   /* Cleanup. */
   unlink(path);
 
